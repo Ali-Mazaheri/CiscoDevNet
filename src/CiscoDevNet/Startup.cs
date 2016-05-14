7ef398c7aa8e0be6f6a7bfe5edbefcd41e945345
Ali-Mazaheri <alimazaheri@yahoo.com>
2016-05-14 10:43:00 -0400

    Initial

--- src/CiscoDevNet/Startup.cs
+++ src/CiscoDevNet/Startup.cs
@@ -15,12 +15,44 @@ namespace CiscoDevNet
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddCors();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.UseIISPlatformHandler();
+
+            //app.Use(async (c, v) => { await v(); });
+            app.UseCors(bu => bu.AllowAnyOrigin());
+            app.UseWebSockets();
+
+            //app.Use(async (http, next) =>
+            //{
+            //    if (http.WebSockets.IsWebSocketRequest)
+            //    {
+            //        var webSocket = await http.WebSockets.AcceptWebSocketAsync();
+            //        if (webSocket != null && webSocket.State == WebSocketState.Open)
+            //        {
+
+            //            // TODO: Handle the socket here.
+            //        }
+            //    }
+            //    else
+            //    {
+            //        // Nothing to do here, pass downstream.  
+            //        await next();
+            //    }
+            //});
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "def",
+                    template: "{controller=Home}/{action=Index}"
+                    );
+            });
+
 
             app.Run(async (context) =>
             {
